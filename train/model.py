d7dd3081ff83f990175b2bf0f6478b4b0fc1234b
connormcmonigle <connormcmonigle@gmail.com>
2021-01-10 19:38:07 -0800

    small net

--- train/model.py
+++ train/model.py
@@ -9,13 +9,12 @@ import util
 class NNUE(nn.Module):
   def __init__(self):
     super(NNUE, self).__init__()
-    BASE = 128
+    BASE = 32
     self.white_affine = nn.Linear(util.half_input_numel(), BASE)
     self.black_affine = nn.Linear(util.half_input_numel(), BASE)
     self.fc0 = nn.Linear(2*BASE, 32)
     self.fc1 = nn.Linear(32, 32)
-    self.fc2 = nn.Linear(64, 32)
-    self.fc3 = nn.Linear(96, 1)
+    self.fc2 = nn.Linear(64, 1)
     
   def forward(self, pov, white, black):
     w_ = self.white_affine(util.half_input(white, black))
@@ -23,8 +22,7 @@ class NNUE(nn.Module):
     base = F.relu(pov * torch.cat([w_, b_], dim=1) + (1.0 - pov) * torch.cat([b_, w_], dim=1))
     x = F.relu(self.fc0(base))
     x = torch.cat([x, F.relu(self.fc1(x))], dim=1)
-    x = torch.cat([x, F.relu(self.fc2(x))], dim=1)
-    x = self.fc3(x)
+    x = self.fc2(x)
     return x
 
   def to_binary_file(self, path):
